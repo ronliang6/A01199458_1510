c339053b1cb0b5f72e5d0b3cef00d719d462dea6
ronliang6 <ronaldliang889@gmail.com>
2020-01-21 08:56:41 -0800

    Fixing grammar and refactored program name

--- Lab02/lab02.py
+++ Lab02/lab02.py
@@ -40,7 +40,7 @@ def main():
     then call create_name function."""
 
     print("Welcome to my dice rolling program! You can enter the number of dice that you are rolling as well as how "
-          "many sides you want the dice to have.")
+          "many sides you want the dice to have")
     number_of_rolls = input("Please enter the number of dice that you wish to roll")
     number_of_sides = input("Please enter the number of sides that you wish your dice to have")
 
