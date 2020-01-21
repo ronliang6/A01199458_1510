ec6ca7b5c5b84cf0af9fa49ade5805d4ca551054
ronliang6 <ronaldliang889@gmail.com>
2020-01-20 19:56:55 -0800

    Added main method with errors. Added if name = main block to call main.

---
+++ Lab02/errors.py
@@ -0,0 +1,17 @@
+def main():
+    """Generate a ZeroDivisionError, two IndexErrors, and two TypeErrors."""
+
+    print(1 / 0)
+
+    error_list = ["one", "two", "three"]
+    print(error_list[len(error_list)])
+    print(error_list[-4])
+
+    print("Hello, I am " + 21)
+    weeks_in_year = 365 / "7"
+
+
+if __name__ == "__main__":
+    """Call the main method if this is the main program."""
+
+    main()
