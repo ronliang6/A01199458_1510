9195a6e6175468a7fca567b02737fe1903a90e54
ronliang6 <ronaldliang889@gmail.com>
2020-01-20 23:55:29 -0800

    Created create_name function and added a call statement into the main function.

--- Lab02/Lab02.py
+++ Lab02/Lab02.py
@@ -1,4 +1,17 @@
 import random
+import string
+
+
+def create_name(length):
+    """Generate a title case string of random letters of a given length."""
+
+    if str.isdigit(str(length)):
+        random_string = ""
+        for x in range(length):
+            random_string += random.choice(string.ascii_letters)
+        return random_string.title()
+    else:
+        return None
 
 
 def roll_die(number_of_rolls, number_of_sides):
@@ -23,12 +36,18 @@ def roll_die(number_of_rolls, number_of_sides):
 
 
 def main():
+    """Ask for arguments to pass onto roll_die function, then call roll_die function with those arguments,
+    then call create_name function."""
+
     print("Welcome to my dice rolling program! You can enter the number of dice that you are rolling as well as how "
           "many sides you want the dice to have.")
     number_of_rolls = input("Please enter the number of dice that you wish to roll")
     number_of_sides = input("Please enter the number of sides that you wish your dice to have")
+
     print("You rolled: " + str(roll_die(number_of_rolls, number_of_sides)) + ". Hope it was good. "
                                                                              "Unless you're the DM.")
+
+    print("Your random string is: " + create_name(15))
 
 
 if __name__ == "__main__":
