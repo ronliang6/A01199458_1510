f41fbd425a818091427d88d761d1ddac9133e284
ronliang6 <ronaldliang889@gmail.com>
2020-01-25 16:35:55 -0800

    added helper function, docstrings needed

--- a1/assignment_one.py
+++ a1/assignment_one.py
@@ -1,15 +1,19 @@
 def convert_to_roman_numeral(positive_int):
     length = len(str(positive_int))
     roman_numeral = ""
+    for i in range(length):
+        roman_numeral = digit_converter(positive_int, length, i) + roman_numeral
+    return roman_numeral
+
+
+def digit_converter(positive_int, length, i):
     roman_numeral_list = [["", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"],
                           ["", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"],
                           ["", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"],
                           ["", "M", "MM", "MMM", "MMMM", "MMMMM", "MMMMMM", "MMMMMMM", "MMMMMMMM", "MMMMMMMMM"],
-                          ["", "MMMMMMMMMM",]]
+                          ["", "MMMMMMMMMM"]]
 
-    for i in range(length):
-        roman_numeral = roman_numeral_list[i][int(str(positive_int)[-(i+1)])] + roman_numeral
-    return roman_numeral
+    return roman_numeral_list[i][int(str(positive_int)[-(i+1)])]
 
 
 def main():
