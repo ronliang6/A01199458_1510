f0333733a57ce9efe8c81bdfaca0c13354099346
ronliang6 <ronaldliang889@gmail.com>
2020-01-28 15:04:20 -0800

    updated variable names

---
+++ Lab03/dijkstra.py
@@ -0,0 +1,60 @@
+"""
+Sort a list of colours into the order red, white, and blue.
+"""
+
+import doctest
+
+
+def dijkstra(colour_list):
+    """
+    Sort a list of red, white, or blue.
+
+    :param colour_list: a list of strings that only contain the values "red", "white", or "blue".
+    :precondition: the function is called with acceptable PARAM statements as defined above.
+    :postcondition: sort the list given into the order of red, then white, then blue.
+
+    >>> dijkstra(["red", "blue", "red"])
+    ['red', 'red', 'blue']
+    >>> dijkstra(["red"])
+    ['red']
+    >>> dijkstra(["white", "red", "blue", "white", "red", "blue", "red", "blue", "white"])
+    ['red', 'red', 'red', 'white', 'white', 'white', 'blue', 'blue', 'blue']
+    >>> dijkstra(["red", "white", "blue"])
+    ['red', 'white', 'blue']
+    """
+    colour_list.sort(key=dutch_sort)
+    print(colour_list)
+
+
+def dutch_sort(colour):
+    """
+    Assign a value to certain colours.
+
+    :param colour: a string that has the value "red", "white", or "blue".
+    :precondition: the function is called with acceptable PARAM statements as defined above.
+    :postcondition: return an object defined below in the RETURN statement.
+    :return: return an integer representing the value of the string given as an argument.
+
+    >>> dutch_sort("red")
+    1
+    >>> dutch_sort("white")
+    2
+    >>> dutch_sort("blue")
+    3
+    """
+    if colour == "red":
+        return 1
+    if colour == "white":
+        return 2
+    if colour == "blue":
+        return 3
+
+
+def main():
+    doctest.testmod()
+    dutch = ["white", "red", "blue", "white", "red", "blue", "red", "blue", "white"]
+    dijkstra(dutch)
+
+
+if __name__ == "__main__":
+    main()
