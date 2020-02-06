9bd932798ad215b630cd4028f3ba54237455f721
ronliang6 <ronaldliang889@gmail.com>
2020-02-06 01:22:43 -0800

    Finished docstrings and added readability improvements to functions.

--- Lab04/lab04.py
+++ Lab04/lab04.py
@@ -6,26 +6,32 @@ def eratosthenes(upperbound):
     """
     Return a list of all prime numbers less than the upper bound provided.
 
-    :param upperbound: a positive integer
-    :return: a list of positive integers
+    :param upperbound: A positive integer.
+    :precondition: Enter valid arguments according to the PARAM statement above.
+    :postcondition: Return a list of all the prime numbers less than the upperbound parameter.
+    :return: A list of positive integers.
 
+    >>> eratosthenes(1)
+    []
+    >>> eratosthenes(3)
+    [2]
     >>> eratosthenes(10)
     [2, 3, 5, 7]
-
     >>> eratosthenes(100)
     [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97]
     """
     numbers_list = list(range(2, upperbound))
     position = 0
 
-    while numbers_list[position] < math.sqrt(upperbound):
-        numbers_list = eratosthenes_helper(numbers_list, numbers_list[position])
-        position += 1
+    if upperbound > 2:
+        while numbers_list[position] < math.sqrt(upperbound):
+            numbers_list = delete_multiples_of(numbers_list, numbers_list[position])
+            position += 1
 
     return numbers_list
 
 
-def eratosthenes_helper(numbers_list, prime):
+def delete_multiples_of(numbers_list, prime):
     numbers_to_delete = []
 
     for i in range(len(numbers_list)):
@@ -40,16 +46,17 @@ def eratosthenes_helper(numbers_list, prime):
 
 def cash_money(money):
     """
+    Calculate the largest denominations of bills and coins that can represent the money inputted as a float.
 
-    :param money:
-    :return:
+    :param money: A positive float.
+    :precondition: Provide the function with a valid argument as defined by the PARAM statement above.
+    :postcondition: Return an object as defined by the return statement below.
+    :return: A list of eleven integers representing the number of each denomination.
 
     >>> cash_money(0.01)
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1]
-
-    >>> cash_money(66.53)
-    [0, 1, 0, 1, 1, 0, 1, 2, 0, 0, 3]
-
+    >>> cash_money(88.68)
+    [0, 1, 1, 1, 1, 1, 1, 2, 1
     >>> cash_money(9999999.99)
     [99999, 1, 2, 0, 1, 2, 0, 3, 2, 0, 4]
     """
