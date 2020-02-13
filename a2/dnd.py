e33a9f9ea32a2ee872345c8fdcf89fc05bfe5cd8
ronliang6 <ronaldliang889@gmail.com>
2020-02-12 23:29:18 -0800

    updated several functions to reflect new requirements, added documentation

--- a2/dnd.py
+++ a2/dnd.py
@@ -11,6 +11,13 @@ def roll_die(number_of_rolls, number_of_sides):
     :precondition: provide valid arguments to the function as defined by the PARAM statements above.
     :postcondition: return an object as defined by the return statement below.
     :return: a positive integer representing the sum of the dice rolls.
+
+    >>> roll_die(1, 1)
+    1
+    >>> roll_die(15, 1)
+    15
+    >>> roll_die(9999, 1)
+    9999
     """
     dice_sum = 0
     for i in range(number_of_rolls):
@@ -50,23 +57,40 @@ def generate_vowel():
 
     :precondition: provide the function with no arguments.
     :postcondition: return an object as defined by the return statement below.
-    :return: a string of length one that is a vowel (including y).
+    :return: a string of length one that is a vowel (including "y").
     """
     return random.choice("aeiouy")
 
 
 def generate_consonant():
     """
-    Generate and return a random consonant (including y).
+    Generate and return a random consonant (including "y").
 
     :precondition: provide the function with no arguments.
     :postcondition: return an object as defined by the return statement below.
-    :return: a string of length one that is a consonant (including y).
+    :return: a string of length one that is a consonant (including "y").
     """
     return random.choice("bcdfghjklmnpqrstvwxyz")
 
 
 def create_character(syllables):
+    """
+    Create a properly formatted Dungeons and Dragons character as a dictionary and return it.
+
+    A properly formatted Dungeons and Dragons character is a dictionary object that includes (as values):
+    a name (string),
+    six statistics (integers in the range [3,18],
+    an inventory (a list),
+    XP (an integer),
+    a class (a string),
+    a race (a string), and
+    HP (an integer).
+    The name, statistics, and HP have random elements in their creation. The inventory and XP are initialized as the
+    same value every time. The class and race are chosen from a list of predefined values.
+
+    :param syllables: a positive integer representing the desired syllables in the character name.
+    :return: a 
+    """
     if not syllables.isdigit() or int(syllables) <= 0:
         print("You have failed to enter a positive integer.")
     else:
@@ -82,7 +106,7 @@ def create_character(syllables):
                      "XP": 0}
         print("Your character so far is: ")
         print_character(character)
-        character["Class"] = select_class(),
+        character["Class"] = select_class()
         character["Race"] = select_race()
         character_health = roll_hit_dice(character["Class"])
         character["HP"] = [character_health, character_health]
@@ -116,9 +140,20 @@ def select_class():
     :postcondition: return an object as defined by the return statement below.
     :return: a string representing the class of the character.
     """
-    character_class = input("Please pick a class from the following list: "
-                            "Fighter, Rogue, Monk, Ranger, Wizard, Warlock, Sorcerer, Cleric, Druid, Bard, Barbarian, "
-                            "and Paladin.")
+    classes = ["Fighter", "Rogue", "Monk", "Ranger", "Wizard", "Warlock", "Sorcerer", "Cleric", "Druid", "Bard",
+               "Barbarian", "Paladin"]
+    for i in range(len(classes)):
+        print(str(i+1) + " : " + classes[i])
+
+    selection = input("Please pick a class by entering that classes' corresponding integer.")
+
+    while True:
+        if selection.isdigit() and int(selection) in range(1, 13):
+            break
+        selection = input("You have entered an invalid input, please enter an integer in the range [1,12]")
+
+    character_class = classes[int(selection)-1]
+    print(character_class)
     return character_class.lower()
 
 
@@ -130,8 +165,20 @@ def select_race():
     :postcondition: return an object as defined by the return statement below.
     :return: a string representing the race of the character.
     """
-    character_race = input("Please pick a class from the following list: "
-                           "Tiefling, Dragonborn, Human, Half-Elf, Elf, Dwarf, Gnome, Halfling, and Half-Orc.")
+
+    races = ["Tiefling", "Dragonborn", "Human", "Half-Elf", "Elf", "Dwarf", "Gnome", "Halfling", "Half-Orc"]
+    for i in range(len(races)):
+        print(str(i+1) + " : " + races[i])
+
+    selection = input("Please pick a race by entering that race's corresponding integer.")
+
+    while True:
+        if selection.isdigit() and int(selection) in range(1, 10):
+            break
+        selection = input("You have entered an invalid input, please enter an integer in the range [1,9]")
+
+    character_race = races[int(selection)-1]
+    print(character_race)
     return character_race.lower()
 
 
@@ -157,26 +204,34 @@ def choose_inventory(character):
     
     So here are some non-potion wares!
     
-    1. Chris Thompson
-    2. Ring of Three Wishes
+    1. Ring of Three Wishes
+    2. Chris Thompson's Shorts (his favorite beige pair)
     3. Twelve Mostly-Empty Xerox Printer Cartridges
     4. Your Local Politician
     5. Potion Seller's Store
+    6. Executive Costco Membership
+    7. A Spherical Muffin Where There Is Only Top-Muffin
+    8. 0.000176 bitcoin
+    9. The Midterm Answers
+    10. A Bonus Mark
     
     What would you like to buy?
     (Please enter a list number to buy that item or -1 to leave my store)"""
 
-    items = ["Chris Thompson", "Ring of Three Wishes", "Twelve Mostly-Empty Xerox Printer Cartridges",
-             "Your Local Politician", "Potion Seller's Store"]
+    items = ["Ring of Three Wishes", "Chris Thompson's Shorts (his favorite beige pair)",
+             "Twelve Mostly-Empty Xerox Printer Cartridges",
+             "Your Local Politician", "Potion Seller's Store", "Executive Costco Membership",
+             "A Spherical Muffin Where There Is Only Top-Muffin",
+             "0.000176 bitcoin", "The Midterm Answers", "A Bonus Mark"]
     shopping_basket = []
 
     purchase = input(shop_message)
     while purchase != "-1":
-        if purchase.isdigit() and int(purchase) in [1, 2, 3, 4, 5]:
+        if purchase.isdigit() and int(purchase) in list(range(1, 11)):
             shopping_basket.append(items[int(purchase) - 1])
             purchase = input(shop_message)
         else:
-            print("Please enter an integer in the range [1,5] or -1.")
+            print("Please enter an integer in the range [1,10] or -1.")
             purchase = input(shop_message)
     character["Inventory"].extend(shopping_basket)
 
