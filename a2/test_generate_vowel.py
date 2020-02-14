81cab953ff441f67e6df61983d916af441ff9d3e
ronliang6 <ronaldliang889@gmail.com>
2020-02-14 12:39:08 -0800

    added a unit test for generate_vowel

--- a2/test_generate_vowel.py
+++ a2/test_generate_vowel.py
@@ -1,12 +1,12 @@
 from unittest import TestCase
 
-from a2.dnd import generate_consonant
+from a2.dnd import generate_vowel
 
 
 class Test(TestCase):
-    def test_generate_consonant_many_times(self):
+    def test_generate_vowel_many_times(self):
         does_pass = True
         for _ in range(1000):
-            if generate_consonant() not in "bcdfghjklmnpqrstvwxyz":
+            if generate_vowel() not in "aeiouy":
                 does_pass = False
         self.assertTrue(does_pass)
