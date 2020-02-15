a19f5b69b9bc97adc36b989d307df7581c268e04
ronliang6 <ronaldliang889@gmail.com>
2020-02-15 15:36:53 -0800

    fixed print_character unit test

--- a2/test_print_character.py
+++ a2/test_print_character.py
@@ -7,20 +7,38 @@ import dnd
 
 class Test(TestCase):
     @patch('sys.stdout', new_callable=io.StringIO)
-    def test_print_character(self, output):
-        actual = dnd.print_character({"Name": "Solar",
-                                      "Strength": 26,
-                                      "Intelligence": 25,
-                                      "Wisdom": 25,
-                                      "Dexterity": 14,
-                                      "Constitution": 26,
-                                      "Charisma": 30,
-                                      "Inventory": ["Angelic Weapons", "Divine Awareness", "Innate Spellcasting"],
-                                      "XP": 33000,
-                                      "Class": "Paladin",
-                                      "Race": "human",
-                                      "HP": [35, 35]})
-        expected = "Name: Solar\nStrength: 26\nIntelligence:\n25 Wisdom: 25\nDexterity: 14\nConstitution: 26\n" \
+    def test_print_character_with_items(self, output):
+        dnd.print_character({"Name": "Solar",
+                             "Strength": 26,
+                             "Intelligence": 25,
+                             "Wisdom": 25,
+                             "Dexterity": 14,
+                             "Constitution": 26,
+                             "Charisma": 30,
+                             "Inventory": ["Angelic Weapons", "Divine Awareness", "Innate Spellcasting"],
+                             "XP": 33000,
+                             "Class": "Paladin",
+                             "Race": "human",
+                             "HP": [35, 35]})
+        expected = "Name: Solar\nStrength: 26\nIntelligence: 25\nWisdom: 25\nDexterity: 14\nConstitution: 26\n" \
                    "Charisma: 30\nInventory:\nAngelic Weapons\nDivine Awareness\nInnate Spellcasting\nXP: 33000\n" \
-                   "Class: Paladin\nRace: human\nHP: [35, 35]"
+                   "Class: Paladin\nRace: human\nHP: [35, 35]\n"
+        self.assertEqual(output.getvalue(), expected)
+
+    @patch('sys.stdout', new_callable=io.StringIO)
+    def test_print_character_no_items(self, output):
+        dnd.print_character({"Name": "Solar",
+                             "Strength": 26,
+                             "Intelligence": 25,
+                             "Wisdom": 25,
+                             "Dexterity": 14,
+                             "Constitution": 26,
+                             "Charisma": 30,
+                             "Inventory": [],
+                             "XP": 33000,
+                             "Class": "Paladin",
+                             "Race": "human",
+                             "HP": [35, 35]})
+        expected = "Name: Solar\nStrength: 26\nIntelligence: 25\nWisdom: 25\nDexterity: 14\nConstitution: 26\n" \
+                   "Charisma: 30\nInventory:\nXP: 33000\nClass: Paladin\nRace: human\nHP: [35, 35]\n"
         self.assertEqual(output.getvalue(), expected)
