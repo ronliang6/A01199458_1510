777cac4d0c92f1c675da44d2a74f23970c599e2f
ronliang6 <ronaldliang889@gmail.com>
2020-02-15 17:27:26 -0800

    added a testcase for roll_to_hit

--- a2/test_roll_to_hit.py
+++ a2/test_roll_to_hit.py
@@ -66,3 +66,34 @@ class Test(TestCase):
         actual = defender["HP"][1]
         expected = 5
         self.assertEqual(actual, expected)
+
+    @patch('dnd.roll_die', side_effect=[14])
+    def test_roll_to_hit_roll_equals_dexterity(self, _):
+        attacker = {"Name": "Solar",
+                    "Strength": 26,
+                    "Intelligence": 25,
+                    "Wisdom": 25,
+                    "Dexterity": 14,
+                    "Constitution": 26,
+                    "Charisma": 30,
+                    "Inventory": [],
+                    "XP": 33000,
+                    "Class": "paladin",
+                    "Race": "human",
+                    "HP": [35, 5]}
+        defender = {"Name": "Solar2",
+                    "Strength": 26,
+                    "Intelligence": 25,
+                    "Wisdom": 25,
+                    "Dexterity": 14,
+                    "Constitution": 26,
+                    "Charisma": 30,
+                    "Inventory": [],
+                    "XP": 33000,
+                    "Class": "paladin",
+                    "Race": "human",
+                    "HP": [35, 5]}
+        dnd.roll_to_hit(attacker, defender)
+        actual = defender["HP"][1]
+        expected = 5
+        self.assertEqual(actual, expected)
