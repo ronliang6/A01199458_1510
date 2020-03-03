47bb7c4535efa40bd92f26f06b8aa58e4564764f
ronliang6 <ronaldliang889@gmail.com>
2020-03-03 15:18:32 -0800

    added display_position function

--- Lab06/maze.py
+++ Lab06/maze.py
@@ -10,6 +10,16 @@ def create_character() -> list:
     return [0, 0]
 
 
+def display_position(game_board: list, character: list):
+    print("You are currently here:")
+    for position in game_board:
+        if position[0] == character[0] and position[1] == character[1]:
+            print('C', end=" ")
+        else:
+            print("*", end=" ")
+        if position[1] == 4:
+            print("")
+
 
 
 
