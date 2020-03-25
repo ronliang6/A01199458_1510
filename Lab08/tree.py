632937bb34593a27ebe8de0f801c15651285cf0d
ronliang6 <ronaldliang889@gmail.com>
2020-03-25 16:54:14 -0700

    added docstrings for setters.

--- Lab08/tree.py
+++ Lab08/tree.py
@@ -71,21 +71,63 @@ class Tree:
         """
         return self.__circumference_cm
 
-    def set_species(self, species: str):
-        if species.replace(" ", ""):
-            self.__species = species
+    def set_species(self, new_species: str):
+        """
+        Set the species instance variable of a tree object to new_species.
+
+        :param new_species: a string.
+        :precondition: provide the function with an argument as defined by the PARAM statement above.
+        :postcondition: set the species instance variable of the tree object to new_species or raise an error as
+        explained below in the raise error statement.
+        :raise ValueError: if new_species does not contain at least one non-whitespace character.
+
+        >>> tree_one = Tree("Douglas-fir", 20, 31.23)
+        >>> tree_one.set_species("Western Red Cedar")
+        >>> tree_one.get_species()
+        'Western Red Cedar'
+        """
+        if new_species.replace(" ", ""):
+            self.__species = new_species
         else:
             raise ValueError("species requires at least one non-whitespace character.")
 
-    def set_age_years(self, age_years: int):
-        if age_years >= 0:
-            self.__age_years = age_years
+    def set_age_years(self, new_age_years: int):
+        """
+        Set the age_years instance variable of a tree object to new_age_years.
+
+        :param new_age_years: an int.
+        :precondition: provide the function with an argument as defined by the PARAM statement above.
+        :postcondition: set the age_years instance variable of the tree object to new_age_years or raise an error as
+        explained below in the raise error statement.
+        :raise ValueError: if new_age_years is less than 0.
+
+        >>> tree_one = Tree("Douglas-fir", 20, 31.23)
+        >>> tree_one.set_age_years(54)
+        >>> tree_one.get_age_years()
+        54
+        """
+        if new_age_years >= 0:
+            self.__age_years = new_age_years
         else:
             raise ValueError("age_years must be at least 0.")
 
-    def set_circumference_cm(self, circumference_cm: float):
-        if circumference_cm >= 0:
-            self.__circumference_cm = circumference_cm
+    def set_circumference_cm(self, new_circumference_cm: float):
+        """
+        Set the circumference_cm instance variable of a tree object to new_circumference_cm.
+
+        :param new_circumference_cm: a float.
+        :precondition: provide the function with an argument as defined by the PARAM statement above.
+        :postcondition: set the circumference_cm instance variable of the tree object to new_circumference_cm or
+        raise an error as explained below in the raise error statement.
+        :raise ValueError: if new_circumference_cm is less than 0.
+
+        >>> tree_one = Tree("Douglas-fir", 20, 31.23)
+        >>> tree_one.set_circumference_cm(23.61)
+        >>> tree_one.get_circumference_cm()
+        23.61
+        """
+        if new_circumference_cm >= 0:
+            self.__circumference_cm = new_circumference_cm
         else:
             raise ValueError("circumference_cm must be at least 0.")
 
