a3652f39937be87ff7c18876e859d3792546c02f
ronliang6 <ronaldliang889@gmail.com>
2020-04-11 22:52:58 -0700

    some documentation, formatting, reformatting, implemented start of while loop

--- Lab11/final_frontier.py
+++ Lab11/final_frontier.py
@@ -2,10 +2,34 @@ import tkinter as tk
 from PIL import ImageTk, Image
 import requests
 import random
+import textwrap
 
 
-def fun():
-    # nasa api call for daily image with random date
+def print_nasa_explanation(data: dict):
+    """
+    Print interesting information about a daily nasa picture.
+
+    :param data: a dictionary representing a parsed response from the NASA picture of the day API.
+    :precondition: provide the function with an argument as defined by the PARAM statement above.
+    :postcondition: print interesting information about the picture.
+    """
+    print(f'The image you see is titled: {data["title"]}.')
+    print(f'It is the picture of the day from {data["date"]}!')
+    print('Here is some cool information about that image!\n')
+    formatted_text = textwrap.wrap(data["explanation"], width=100)
+    for line in formatted_text:
+        print(line)
+
+
+def nasa_query() -> dict:
+    """
+    Get, parse, and return a date-randomized response from the NASA APOD API.
+
+    :precondition: the NASA APOD API is fully functional. The API key remains valid.
+    :postcondition: return an object as defined by the return statement below.
+    :return: a dictionary representing a parsed response from the NASA picture of the day API.
+    """
+    # api query url assembly
     year = random.randint(1996, 2019)
     month = random.randint(1, 12)
     day = random.randint(1, 28)
@@ -13,24 +37,35 @@ def fun():
     key = 'wtxdhlKb5seLp282a6Dsd3jofPYpcTdYiKupDaXC'
     config = f'&date={year}-{month}-{day}'
     url = root + key + config
+
     response = requests.get(url)
-    data = response.json()
+    return response.json()
 
-    # tkinter GUI
-    top = tk.Tk()
-    canvas = tk.Canvas(top, width=800, height=800)
-    print(data['url'])
 
-    response = requests.get(data['url'], stream=True)
-    with Image.open(response.raw) as img:
+def display_nasa_image(image):
+    """
+    Display an image in a tkinter window for 30 seconds, then close the window.
+
+    :param image: a response object representing a streamed image file.
+    :precondition: provide the function with an argument as defined by the PARAM statement above.
+    :postcondition: open a tkinter window with an image in it for 30 seconds, then close that window.
+    """
+    tkinter_gui = tk.Tk()
+    with Image.open(image.raw) as img:
         tk_img = ImageTk.PhotoImage(img)
+    canvas = tk.Canvas(tkinter_gui, width=tk_img.width(), height=tk_img.height())
     canvas.create_image(0, 0, anchor='nw', image=tk_img)
     canvas.pack()
-    top.mainloop()
+    tkinter_gui.after(30000, lambda: tkinter_gui.destroy())
+    tkinter_gui.mainloop()
 
 
 def main():
-    fun()
+    while True:
+        nasa_response = nasa_query()
+        print_nasa_explanation(nasa_response)
+        nasa_image = requests.get(nasa_response['url'], stream=True)
+        display_nasa_image(nasa_image)
 
 
 if __name__ == "__main__":
