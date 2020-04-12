7aeba894c23fa4f046fbb281f22a718f83f67149
ronliang6 <ronaldliang889@gmail.com>
2020-04-11 18:02:53 -0700

    randomized get request date, installed, imported, and implemented prototype of tkinter and PIL for gui, working version image display.

--- Lab11/final_frontier.py
+++ Lab11/final_frontier.py
@@ -1,15 +1,32 @@
-import tkinter
+import tkinter as tk
+from PIL import ImageTk, Image
 import requests
+import random
 
 
 def fun():
-    config = '/search?q=moon'
-    root = 'https://images-api.nasa.gov'
-    url = root + config
-    print(url)
+    # nasa api call for daily image with random date
+    year = random.randint(1996, 2019)
+    month = random.randint(1, 12)
+    day = random.randint(1, 28)
+    root = 'https://api.nasa.gov/planetary/apod?api_key='
+    key = 'wtxdhlKb5seLp282a6Dsd3jofPYpcTdYiKupDaXC'
+    config = f'&date={year}-{month}-{day}'
+    url = root + key + config
     response = requests.get(url)
     data = response.json()
-    print(data['collection']['items'][0]['data'][0])
+
+    # tkinter GUI
+    top = tk.Tk()
+    canvas = tk.Canvas(top, width=800, height=800)
+    print(data['url'])
+
+    response = requests.get(data['url'], stream=True)
+    with Image.open(response.raw) as img:
+        tk_img = ImageTk.PhotoImage(img)
+    canvas.create_image(0, 0, anchor='nw', image=tk_img)
+    canvas.pack()
+    top.mainloop()
 
 
 def main():
